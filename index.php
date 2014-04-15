9c2a456b33df0a33d001099db3674de59ddd6c3a
Patrick Heck <patrick@patrickheck.de>
2014-04-15 14:10:32 +0200

    add some comments

--- index.php
+++ index.php
@@ -1,30 +1,35 @@
 <?php 
-/*
-	Pixel-Gyro 1.1
-	
-	This script searches for all images in a given directory and displays them as seperate pages.
-	Great for presenting mockups.
-	
-	Published under MIT License
-
-	Copyright (c) 2013 by Patrick Heck (patrick@patrickheck.de)
-	
-	Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
-	associated documentation files (the "Software"), to deal in the Software without restriction,
-	including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
-	and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
-	subject to the following conditions:
-	
-	The above copyright notice and this permission notice shall be included in all copies or substantial
-	portions of the Software.
-	
-	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
-	LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-	IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-	WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-*/
+/**
+ *	Pixel-Gyro
+ *	
+ *	This script searches for all images in a given directory and displays them as seperate pages.
+ *	Great for presenting mockups.
+ *	
+ *	Published under MIT License
+ *
+ *	Copyright (c) 2013 by Patrick Heck (patrick@patrickheck.de)
+ *	
+ *	Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ *	associated documentation files (the "Software"), to deal in the Software without restriction,
+ *	including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ *	and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+ *	subject to the following conditions:
+ *	
+ *	The above copyright notice and this permission notice shall be included in all copies or substantial
+ *	portions of the Software.
+ *	
+ *	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+ *	LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ *	IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ *	WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * @version 1.1
+ * @author Patrick Heck (patrick@patrickheck.de)
+ * @copyright Copyright (c) 2013 by Patrick Heck (patrick@patrickheck.de)
+ * @license http://opensource.org/licenses/MIT
+ * @link https://github.com/patrickheck/pixel-gyro
+ */
 
 // You can set your own constants in a file called config.php
 // otherwise defaults will be loaded below
