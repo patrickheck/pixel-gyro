70f0d5674b2bf3180302b92be0587c523c3f0b4a
Patrick Heck <patrick@patrickheck.de>
2014-04-16 12:31:57 +0200

    add titles

--- index.php
+++ index.php
@@ -1,28 +1,28 @@
 <?php 
 /**
- *	Pixel-Gyro
- *	
- *	This script searches for all images in a given directory and displays them as seperate pages.
- *	Great for presenting mockups.
- *	
- *	Published under MIT License
+ * Pixel-Gyro
+ * 
+ * This script searches for all images in a given directory and displays them as seperate pages.
+ * Great for presenting mockups.
+ * 
+ * Published under MIT License
  *
- *	Copyright (c) 2013 by Patrick Heck (patrick@patrickheck.de)
- *	
- *	Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
- *	associated documentation files (the "Software"), to deal in the Software without restriction,
- *	including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
- *	and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
- *	subject to the following conditions:
- *	
- *	The above copyright notice and this permission notice shall be included in all copies or substantial
- *	portions of the Software.
- *	
- *	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
- *	LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- *	IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- *	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
- *	WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * Copyright (c) 2013 by Patrick Heck (patrick@patrickheck.de)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+ * LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  *
  * @version 1.1
  * @author Patrick Heck (patrick@patrickheck.de)
@@ -136,15 +136,15 @@ if ($n == 0) {
 <div class="navigation dark">
   <div class="container-fluid">
 <div class="col-sm-2 col-xs-4 paging">
-	<a href="<?php echo $_SERVER['PHP_SELF'] . '?n=' . $previndex ?>"><i class="fa fa-arrow-circle-left"></i></a>
+	<a href="<?php echo $_SERVER['PHP_SELF'] . '?n=' . $previndex ?>" title="Previous Image"><i class="fa fa-arrow-circle-left"></i></a>
 	<span class="pagenumber"><?php echo $n+1 ?>/<?php echo sizeof($files) ?></span>
-	<a href="<?php echo $_SERVER['PHP_SELF'] . '?n=' . $nextindex ?>"><i class="fa fa-arrow-circle-right"></i></a>
+	<a href="<?php echo $_SERVER['PHP_SELF'] . '?n=' . $nextindex ?>" title="Next Image"><i class="fa fa-arrow-circle-right"></i></a>
 </div>
 <div class="col-xs-8 col-sm-10">
 	<strong><?=htmlentities(SITENAME,ENT_QUOTES,'UTF-8')?></strong> :
 	
 	<?=htmlentities(basename($imgpath,".".FILEEXTENSION),ENT_QUOTES,'UTF-8')?>
-<button type="button" class="close" aria-hidden="true">&times;</button>
+<button type="button" class="close" aria-hidden="true" title="Hide Toolbar">&times;</button>
 </div>
 </div>
 </div>
